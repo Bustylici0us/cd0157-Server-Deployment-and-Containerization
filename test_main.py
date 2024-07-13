c6136a837145bdd3040d6e42da906eac38435fe7
Bustylici0us <161844907+Bustylici0us@users.noreply.github.com>
2024-07-13 12:01:49 +0200

    Add files via upload

--- test_main.py
+++ test_main.py
@@ -17,7 +17,6 @@ def client():
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
-
     yield client
 
 
@@ -26,6 +25,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    assert False
 
 
 def test_auth(client):
